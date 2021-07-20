8fc5a28f8e2ebcc35c15b3309a83b152901d585b
Yasir Nadeem <m-yasir.dev@protonmail.com>
2021-07-20 17:24:19 +0500

    Comment eventstore subscriptions

--- src/providers/event-bus.provider.ts
+++ src/providers/event-bus.provider.ts
@@ -20,19 +20,19 @@ export const eventStoreBusConfig: EventStoreBusConfig = {
         // TODO: read about subs in eventStore, how can they help us.
         // TODO: dont forget to create a `Persistent Subscription`
         // TODO: and enable `resolveLinkTos` https://eventstore.org/docs/dotnet-api/reading-events/index.html
-        {
-            // persistent subscription
-            type: EventStoreSubscriptionType.Persistent,
-            stream: '$ce-users',
-            persistentSubscriptionName: 'g1',
-        },
-        {
-            // example of persistent subscription to external stream events...
-            type: EventStoreSubscriptionType.Persistent,
-            stream: '$ce-orders',
-            persistentSubscriptionName: 'g1',
-        },
         // {
+        //     // persistent subscription
+        //     type: EventStoreSubscriptionType.Persistent,
+        //     stream: '$ce-users',
+        //     persistentSubscriptionName: 'g1',
+        // },
+        // {
+        //     // example of persistent subscription to external stream events...
+        //     type: EventStoreSubscriptionType.Persistent,
+        //     stream: '$ce-orders',
+        //     persistentSubscriptionName: 'g1',
+        // },
+        // // {
         //     // Catchup subscription
         //     type: EventStoreSubscriptionType.CatchUp,
         //     stream: '$ce-users',
